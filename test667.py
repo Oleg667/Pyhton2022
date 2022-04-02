71a70fb9c96cce3f374e7e99194a6b4ca4fc3572
Oleg667 <oleg-g@yandex.ru>
2022-04-02 18:08:33 +0300

    add file1

--- test667.py
+++ test667.py
@@ -1,4 +1,35 @@
-b=5
-a=2
+#print (120 % 70)
+#print (-120 % 50)
+# f = 653457
+# g = 123493
+# print(f**g)
+# a=1.3
+# print(a)
+# b=2.3
+# print(b)
+# c=a+b
+# print(c)
+# print(a+b)
+# print(1.3+2.3)
+# print(24/4)
+# print(2.4/0.4)
+# print(round((11*2.5/3),2))
+# print(round(3.14159**2/2,0))
+# print (divmod(10, 3))
+# print (divmod(10,-3))
+# stroka=input('enter the numbers separated by a space')
+# stroka1=stroka.split()
+# stroka3='\n'.join(stroka1)
+# print(stroka3)
+#pi = 31.4159265
+#print ("%.4e" % (pi))
+day = 14
+month = 2
+year = 2012
 
-print(c:=b/a)
+print("%d.%02d.%d" % (day, month, year))
+# 14.02.2012
+print("%d-%02d-%d" % (year, month, day))
+# 2012-02-14
+print("%d/%d/%d" % (year, day, month))
+# 2012/14/2
