f24cffbde577d981ef98d5c507e34e07408adb44
Oleg667 <oleg-g@yandex.ru>
2022-04-03 18:40:18 +0300

    add file1

--- test667.py
+++ test667.py
@@ -23,13 +23,47 @@
 # print(stroka3)
 #pi = 31.4159265
 #print ("%.4e" % (pi))
-day = 14
-month = 2
-year = 2012
+# day = 14
+# month = 2
+# year = 2012
+#
+# print("%d.%02d.%d" % (day, month, year))
+# # 14.02.2012
+# print("%d-%02d-%d" % (year, month, day))
+# # 2012-02-14
+# print("%d/%d/%d" % (year, day, month))
+# # 2012/14/2
+# hours=11
+# minutes=2
+# seconds=14
+# cake = ('c','a','k','e')
+# print(type(cake))
+#
+#
+# print("%02d:%02d:%d" % (hours, minutes, seconds))
+# string = input("Введите числа через пробел:")
+#
+# list_of_strings = string.split() # список строковых представлений чисел
+# print(string)
+#
+# list_of_numbers = list(map(int, list_of_strings)) # список чисел
+# a=sum(list_of_numbers[::])
+#
+# pos=len(list_of_numbers)
+# print('в строке значений =',pos)
+# pos1=pos-1
+# print(list_of_numbers[pos1])
+# print(string)
+# print((list_of_numbers[::3]))
+# x=(sum(list_of_numbers[::])) # sum() вычисляет сумму элементов списка
+# x=float(x)
+# print(x)
+# d = {'day' : 22, 'month' : 6, 'year' : 2015}
+#
+# print("||".join(d.keys()))
 
-print("%d.%02d.%d" % (day, month, year))
-# 14.02.2012
-print("%d-%02d-%d" % (year, month, day))
-# 2012-02-14
-print("%d/%d/%d" % (year, day, month))
-# 2012/14/2
+title=input("введите название книги")
+name=input("введите имя автора")
+year=int(input("введите год издания"))
+dict={'title':title,'name':name,'ear':year}
+print (dict)
