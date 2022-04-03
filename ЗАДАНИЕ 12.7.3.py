d903c1642159a378fc0440bf26ecfa3ac0dfdab4
Oleg667 <oleg-g@yandex.ru>
2022-04-03 22:38:18 +0300

    add file1

--- ЗАДАНИЕ 12.7.3.py
+++ ЗАДАНИЕ 12.7.3.py
@@ -1,17 +1,18 @@
 mony=int(input('введите количество денег  >'))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+banks = list(per_cent.keys())
 tkb=(per_cent.get('ТКБ'))*mony/100
 skb=(per_cent.get('СКБ'))*mony/100
 vtb=(per_cent.get('ВТБ'))*mony/100
 sber=(per_cent.get('СБЕР'))*mony/100
 
 deposit = [tkb,skb,vtb,sber]
-# #max_deposit=max(deposit)
-
-
-deposit = [tkb,skb,vtb,sber]
-
 print (list(map(int, deposit)))
+
+
+print(f'\nДоход по депозиту составит:\n{banks[0]} банк - {deposit[0]} рублей\n{banks[1]} банк - {deposit[1]} '
+      f'рублей\n{banks[2]} банк - {deposit[2]} рублей\n{banks[3]} банк - {deposit[3]} рублей\n')
+
 print('Максимальная сумма, которую вы можете заработать — '+str(max(deposit)))
 
 
