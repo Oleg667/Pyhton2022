aa87584893bd670b7ccca3d05e9f49a8641e1bb9
grishaev <oleg-g@yandex.ru>
2022-04-11 18:04:31 +0300

    Merge remote-tracking branch 'origin/master'

--- 2_zadanie 13.8.19.py
+++ 2_zadanie 13.8.19.py
@@ -1,9 +1,9 @@
 #задание 13.8.19
 # запрвшивает количество билетов, проверяет на корректрность ввода.
 # в случае ошибки ввода предлагает повторить ввод или закончить работу
-# вариант первый предусматривал ввод возрастной группы по каждому билету,
-# но если билетов большое количество то это превращается в длительный процесс.
-# поэтому программа запрашивает количество билетов возрастной группы,
+# вариант первый предусматривал ввод возрастной группы по каждому билету (он проще),
+# но если билетов большОе количество, то это превращается в длительный процесс.
+# по этому программа запрашивает количество билетов возрастной группы,
 # в случае если введенное количество больше остатка нераспределенных билетов то
 # считается что все оставшиеся билеты для данной возрастной группы.
 
@@ -39,31 +39,46 @@ raspred=0 # количество расспределенных билетов
 vozrast1=0 # количество посетителей моложе 18 лет
 vozrast2=0 # количество посетителей от 18 до 25
 vozrast3=0 # количество посетителей старше 25
+
+v1 = 0
+v2 = 0
+v3 = 0
+
+
 i=1
 while kol_biletov > raspred:
     # while True:
-        vozrast1 = vvod_int("моложе 18",kol_biletov)
+        vozrast1 = vvod_int("моложе 18",kol_biletov-raspred)
 
-        if  vozrast1 >= kol_biletov:
-            vozrast1=kol_biletov
+        if  vozrast1 >= (kol_biletov-raspred):
+            vozrast1=v1 + (kol_biletov-raspred)
             break
         else:
+            vozrast1 = vozrast1 + v1
             raspred=raspred+vozrast1
             vozrast2 = vvod_int("от 18 до 25", kol_biletov-raspred)
             if vozrast2 >= (kol_biletov-raspred):
-                vozrast2=(kol_biletov-raspred)
+                vozrast2=v2+(kol_biletov-raspred)
                 break
             else:
+                vozrast2 = vozrast1 + v2
                 raspred = raspred + vozrast2
                 vozrast3 = vvod_int("старше 25", kol_biletov-raspred)
                 if vozrast3 >= (kol_biletov - raspred):
-                    vozrast3 = (kol_biletov - raspred)
+                    vozrast3 = v3+(kol_biletov - raspred)
                     break
                 else:
                     raspred = raspred + vozrast3
                     print(f'не распределено {kol_biletov-raspred} билетов')
+                    v1 = vozrast1
+                    v2 = vozrast2
+                    v3 = vozrast3
 
 
+print(raspred)
+print(vozrast1)
+print(vozrast2)
+print(vozrast3)
 
 # блок расчета итоговой суммы
 summa=(vozrast2*990+vozrast3*1390)
