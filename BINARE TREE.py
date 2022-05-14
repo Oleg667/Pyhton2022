18a40fd56a8a96b83dbca17d6b43185d9afc0b86
Oleg667 <oleg-g@yandex.ru>
2022-05-14 19:53:04 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ BINARE TREE.py
@@ -0,0 +1,43 @@
+class Node:  # класс элемента
+    def __init__(self, value=None, next_=None):  # инициализируем
+        self.value = value  # значением
+        self.next = next_  # и ссылкой на следующий элемент
+
+    def __str__(self):
+        return "Node value = " + str(self.value)
+
+
+class LinkedList:  # класс списка
+    def __init__(self):  # инициализируем пустым
+        self.first = None
+        self.last = None
+
+    def clear(self):  # очищаем список
+        self.__init__()
+
+    def __str__(self):  # функция печати
+        R = ''
+
+        pointer = self.first  # берем первый указатель
+        while pointer is not None:  # пока указатель не станет None
+            R += str(pointer.value)  # добавляем значение в строку
+            pointer = pointer.next  # идем дальше по указателю
+            if pointer is not None:  # если он существует добавляем пробел
+                R += ' '
+        return R
+    def pushleft(self, value):#добавляет элемент в начало
+        if self.first is None:
+            self.first = Node(value)
+            self.last = self.first
+        else:
+            new_node = Node(value, self.first)
+            self.first = new_node
+
+    def pushright(self, value):#добавляет элемент в конец
+        if self.first is None:
+            self.first = Node(value)
+            self.last = self.first
+        else:
+            new_node = Node(value)
+            self.last.next = new_node
+            self.last = new_node
