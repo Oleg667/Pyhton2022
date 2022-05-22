4a61fc0daa6341c3affe56c40d2689e3c16f6408
Oleg667 <oleg-g@yandex.ru>
2022-05-22 21:23:06 +0300

    Merge remote-tracking branch 'origin/master'

--- TELE_BOT18.py
+++ TELE_BOT18.py
@@ -1,14 +1,36 @@
 import telebot
 
-#TOKEN = "5366141483:AAEiYvQgf_zeWHU6I4EVzgPMxILNTEAoGMY"
+TOKEN = "5366141483:AAEiYvQgf_zeWHU6I4EVzgPMxILNTEAoGMY"
 
-bot = telebot.TeleBot('5366141483:AAEiYvQgf_zeWHU6I4EVzgPMxILNTEAoGMY')
+bot = telebot.TeleBot(TOKEN)
 
-# @bot.message_handler(content_types=['voice'])
-# def function_name(message: telebot.type.Message):
-#     bot.send_message(message.chat.id, 'У тебя очень красивый голос')
+keys={
+    'биткоин': 'BTC',
+    'эфириум':'ETH',
+    'доллар':'USD'
+}
+
+
+
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, f"Welcome, \ c{message.chat.username}")
+def help(message: telebot.types.Message):
+    text= 'Чтобы начать работать введите команду бота в следующем формате через пробел :\n<имя валюты> \
+<в какую валютуперевести> \
+<количество переводимой валюты>\n Увидеть список всех доступных валют /values '
+    bot.reply_to(message,text)
 
-bot.polling(none_stop=True)
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text ='Доступные валюты'
+    for key in keys.keys():
+       text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['stop'])
+def stop(message: telebot.types.Message):
+    stop=False
+    text = "Ну и вали!"
+    bot.reply_to(message, text)
+
+bot.polling()
+updater.idle()
